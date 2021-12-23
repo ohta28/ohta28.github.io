ddcd9685064c3f24e13893dababa9dc2628ad787
ohta28 <93649322+ohta28@users.noreply.github.com>
2021-12-24 00:50:50 +0900

    Update ビビッドアーミー.js

--- ビビッドアーミー.js
+++ ビビッドアーミー.js
@@ -1,12 +1,21 @@
 //R・SR英雄
 document.getElementById("R_la_001").onclick = function() {
+  var elem = document.getElementById('hero_profile_name').innerHTML = "<p>ティナ</p>";//名前の変更
+
   document.getElementById('hero_images').src='hero_images/R_la_001.png';//画像の変更
   var target = document.getElementById("hero_image_background");//背景画像の変更
   target.className = "hero_image_la";
-  console.log('¯\_(ツ)_/¯');//コンソールログ
-
-  var elem = document.getElementById('hero_profile_name').innerHTML = "<p>ティナ</p>";//名前の変更
+  console.log('3');//コンソールログ
 };
+document.getElementById("R_la_002").onclick = function() {
+  var elem = document.getElementById('hero_profile_name').innerHTML = "<p>ティナ</p>";//名前の変更
+
+  document.getElementById('hero_images').src='hero_images/R_la_001.png';//画像の変更
+  var target = document.getElementById("hero_image_background");//背景画像の変更
+  target.className = "hero_image_se";
+};
+
+
 document.getElementById("R_se_001").onclick = function() {
   document.getElementById('hero_images').src='hero_images/R_se_001.png';//画像の変更
   var target = document.getElementById("hero_image_background");//背景画像の変更
