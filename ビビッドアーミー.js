307af37257a973dcc31638e28db08e1db73ba127
ohta28 <93649322+ohta28@users.noreply.github.com>
2021-12-24 07:28:49 +0900

    Update ビビッドアーミー.js

--- ビビッドアーミー.js
+++ ビビッドアーミー.js
@@ -1,20 +1,17 @@
 //R・SR英雄
 document.getElementById("R_la_001").onclick = function() {
-  var elem = document.getElementById('hero_profile_name').innerHTML = "<p>ティナ</p>";//名前の変更
-
   document.getElementById('hero_images').src='hero_images/R_la_001.png';//画像の変更
   var target = document.getElementById("hero_image_background");//背景画像の変更
   target.className = "hero_image_la";
   console.log('3');//コンソールログ
+
+  var elem = document.getElementById('hero_profile_name').innerHTML = "<p>ティナ</p>";//名前の変更
+  var elem = document.getElementById('hero_profile_skill_name').innerHTML = "<p>致命的な一撃</p>";//名前の変更
 };
 document.getElementById("R_la_002").onclick = function() {
-  var elem = document.getElementById('hero_profile_name').innerHTML = "<p>ティナ</p>";//名前の変更
-
-  document.getElementById('hero_images').src='hero_images/R_la_001.png';//画像の変更
   var target = document.getElementById("hero_image_background");//背景画像の変更
   target.className = "hero_image_se";
 };
-
 
 document.getElementById("R_se_001").onclick = function() {
   document.getElementById('hero_images').src='hero_images/R_se_001.png';//画像の変更
@@ -22,6 +19,7 @@ document.getElementById("R_se_001").onclick = function() {
   target.className = "hero_image_se";
 
   var elem = document.getElementById('hero_profile_name').innerHTML = "<p>マコト</p>";//名前の変更
+  var elem = document.getElementById('hero_profile_skill_name').innerHTML = "<p>全速出撃</p>";//名前の変更
 };
 document.getElementById("R_sk_001").onclick = function() {
   document.getElementById('hero_images').src='hero_images/R_sk_001.png';//画像の変更
@@ -29,4 +27,5 @@ document.getElementById("R_sk_001").onclick = function() {
   target.className = "hero_image_sk";
 
   var elem = document.getElementById('hero_profile_name').innerHTML = "<p>キャロル</p>";//名前の変更
+  var elem = document.getElementById('hero_profile_skill_name').innerHTML = "<p>士気高揚</p>";//名前の変更
 };
